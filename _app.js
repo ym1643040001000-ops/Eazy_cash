96608bfca89b43583484967f6e111660088736e7
ym1643040001000-ops <ym1643040001000@gmail.com>
2025-09-10 20:27:20 +0300

    Rename _app.js to pages/_app.js

--- _app.js
+++
@@ -1,1 +0,0 @@
-import '@/styles/globals.css'; import Nav from '@/components/Nav'; export default function App({Component, pageProps}){ return (<><Nav/><main className='container'><Component {...pageProps}/></main></>); }
---
+++ pages/_app.js
@@ -0,0 +1,1 @@
+import '@/styles/globals.css'; import Nav from '@/components/Nav'; export default function App({Component, pageProps}){ return (<><Nav/><main className='container'><Component {...pageProps}/></main></>); }
